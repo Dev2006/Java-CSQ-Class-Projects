00b5e2dea1757ee536be60015e6d1d97608b4e89
Mohit Sharma <moenavla@gmail.com>
2018-09-29 12:14:47 +0530

    Added file finding project and day 21 java project

---
+++ File finder/src/FileFinder.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import javax.swing.JOptionPane;
+
+public class FileFinder {
+	
+	public static void main(String[] args) {
+		
+		try {
+			File f = new File("C:\\Users\\Mohit Sharma\\Desktop");
+			
+			if(f.isDirectory()) {
+				
+				printDirectoryItems(f);
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printDirectoryItems(File f) {
+		File[] items = f.listFiles();
+		String [] extensions = new String[items.length];
+		
+		//Setting values for unique items
+		for(int i = 0; i < extensions.length; i++) {
+			
+			try {
+				
+				if(items[i].getName().substring(items[i].getName().lastIndexOf('.')) != null) {
+					
+					extensions[i] = items[i].getName().substring(items[i].getName().lastIndexOf('.'));
+				} else {
+					
+					extensions[i] = "";
+				}
+			} catch (StringIndexOutOfBoundsException e) {}
+		}
+		
+		//prints which files you have
+		System.out.println("Files in directory:");
+		for (String i : extensions) System.out.println(i);
+		
+		//asks user which files to show
+		String extensionChoice = JOptionPane.showInputDialog("Which files do you want to view");
+		
+		//Shows the correct files
+		System.out.println("\n" + extensionChoice + " Files:");
+		for (int i = 0; i < items.length; i++) {
+			
+			if(extensions[i].equals(extensionChoice)) {
+				
+				System.out.println(items[i].getName());
+			}
+		}
+	}
+}
